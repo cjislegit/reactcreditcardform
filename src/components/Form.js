dfafc1130d3812a8bc38ecd003c517989a925b0c
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-02-23 15:18:09 -0800

    Added some divs to help layout

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,40 +11,44 @@ const Form = () => {
         <input id='Card Number' type='text' />
         <label for='Card Holder'>Card Holder</label>
         <input id='Card Holder' type='text' />
-        <div>
-          <label>Expiration Date</label>
-          <select>
-            <option value=''>Month</option>
-            <option value='01'>01</option>
-            <option value='02'>02</option>
-            <option value='03'>03</option>
-            <option value='04'>04</option>
-            <option value='05'>05</option>
-            <option value='06'>06</option>
-            <option value='07'>07</option>
-            <option value='08'>08</option>
-            <option value='09'>09</option>
-            <option value='10'>10</option>
-            <option value='11'>11</option>
-            <option value='12'>12</option>
-          </select>
-          <select>
-            <option value=''>Year</option>
-            <option value='2020'>2020</option>
-            <option value='2021'>2021</option>
-            <option value='2022'>2022</option>
-            <option value='2023'>2023</option>
-            <option value='2024'>2024</option>
-            <option value='2025'>2025</option>
-            <option value='2026'>2026</option>
-            <option value='2027'>2027</option>
-            <option value='2028'>2028</option>
-            <option value='2029'>2029</option>
-            <option value='2030'>2030</option>
-          </select>
-          <label for='CVV'>CVV</label>
+        <div className='rowContainer'>
+          <div className='expirationDateContainer'>
+            <label>Expiration Date</label>
+            <select>
+              <option value=''>Month</option>
+              <option value='01'>01</option>
+              <option value='02'>02</option>
+              <option value='03'>03</option>
+              <option value='04'>04</option>
+              <option value='05'>05</option>
+              <option value='06'>06</option>
+              <option value='07'>07</option>
+              <option value='08'>08</option>
+              <option value='09'>09</option>
+              <option value='10'>10</option>
+              <option value='11'>11</option>
+              <option value='12'>12</option>
+            </select>
+            <select>
+              <option value=''>Year</option>
+              <option value='2020'>2020</option>
+              <option value='2021'>2021</option>
+              <option value='2022'>2022</option>
+              <option value='2023'>2023</option>
+              <option value='2024'>2024</option>
+              <option value='2025'>2025</option>
+              <option value='2026'>2026</option>
+              <option value='2027'>2027</option>
+              <option value='2028'>2028</option>
+              <option value='2029'>2029</option>
+              <option value='2030'>2030</option>
+            </select>
+          </div>
+          <div className='cvvContainer'>
+            <label for='CVV'>CVV</label>
+          </div>
+          <input id='CVV' type='text' />
         </div>
-        <input id='CVV' type='text' />
         <button>Submit</button>
       </form>
     </div>
