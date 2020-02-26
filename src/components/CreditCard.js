018eab34673614ae0f2b9ab2703aa4effbeb37a2
cjislegit <carlosjramirez11@gmail.com>
2020-02-26 14:18:06 -0800

    Added placeholder to cc component

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -3,7 +3,13 @@ import React from 'react';
 const CreditCard = () => {
   return (
     <div className='creditCardContainer'>
-      <div className='creditCard'></div>
+      <div className='creditCard'>
+        <div className='chipContainer'></div>
+        <div className='logoContainer'></div>
+        <div className='numContainer'>8888 8888 8888 8888</div>
+        <div className='nameContainer'>FULL NAME</div>
+        <div className='expdateContainer'>MM/YY</div>
+      </div>
     </div>
   );
 };
